f278654cc9d89b3bf7fa1ce64a8f72df083885a2
Sergei Zakharchenko <millennial404@yandex.ru>
2025-02-25 11:41:14 +0300

    change url 0.0.0.0

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,23 +1,20 @@
 export const getAllRooms = async () => {
-  const res = await fetch(" http://127.0.0.1:8000/api/rooms/");
+  const res = await fetch(" http://0.0.0.0:8000/api/rooms/");
   return await res.json();
 };
 
 export const addVolume = async (body: object, id: number) => {
-  const res = await fetch(
-    `http://127.0.0.1:8000/api/rooms/${id}/add-volumes/`,
-    {
-      method: "POST",
-      mode: "cors",
-      cache: "no-cache",
-      credentials: "same-origin",
-      headers: {
-        "Content-Type": "application/json",
-      } as HeadersInit,
-      redirect: "follow",
-      referrerPolicy: "no-referrer",
-      body: JSON.stringify(body),
-    }
-  );
+  const res = await fetch(`http://0.0.0.0:8000/api/rooms/${id}/add-volumes/`, {
+    method: "POST",
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    } as HeadersInit,
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+    body: JSON.stringify(body),
+  });
   return res;
 };
